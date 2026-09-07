bae22b3d221e8aec7b466969cf29c17719fc3438
Muhammad_Zidan_Maulana <maulanamuhammadzidan4@gmail.com>
2026-09-07 12:23:17 +0700

    feat: implement frontend components and core pages

---
+++ src/data/dev.ts
@@ -0,0 +1,17 @@
+export interface Developer {
+    name: string;
+    role: string;
+    description: string;
+    imageUrl: string;
+    githubUrl: string;
+    linkedinUrl: string;
+}
+
+export const dev: Developer = {
+    name: "M. Zidan Maulana",
+    role: "Web Developer",
+    description: "Seorang web developer pemula",
+    imageUrl: "../src/assets/dev.png",
+    githubUrl: "https://github.com/maulanamuhammadzidan4-commits",
+    linkedinUrl: "https://www.linkedin.com/in/m-zidan-maulana-a4a915410/"
+}
